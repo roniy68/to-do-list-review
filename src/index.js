86b3c6eccf0099aa16f6bfafc4ca6a2a43772d92
roniy68 <ahmed.11900449@lpu.in>
2023-01-23 14:28:11 +0530

    Review Branch

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { renderToDoList } from './modules/listUtils.js';
+import toDoTasks from './toDoTasks.js';
+import './modules/eventListeners.js';
+import './styles/style.css';
+
+renderToDoList(toDoTasks);
